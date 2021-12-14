b4c8378295f18c0ec367de49b4148d730f9b2da2
chumingo <montecinosa5@gmail.com>
2021-12-14 02:39:31 -0300

    sk

--- servidores/sv1.go
+++ servidores/sv1.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	f, _ := os.OpenFile("zarkoPichula.txt", os.O_APPEND, 0666)
+	f, _ := os.OpenFile("zarkoPichula.txt", os.O_APPEND|os.O_CREATE, 0666)
 
 	f.WriteString("pichula")
 	f.Close()
